54afb2eb7a842756d442e91530c5fece42882d73
mountainguy76935 <stoll33david@gmail.com>
2019-11-18 15:56:51 -0700

    Updates

---
+++ precache-manifest.36bc2064f6b682de6fb295e7768d8605.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4cf179856b84fc1735997c97c17cb3",
+    "url": "/tip_out_calc/index.html"
+  },
+  {
+    "revision": "3b37c7f0a1d10edee5de",
+    "url": "/tip_out_calc/static/css/main.d83f86bc.chunk.css"
+  },
+  {
+    "revision": "3ba5f41580ffa9729ac8",
+    "url": "/tip_out_calc/static/js/2.77aa3cb3.chunk.js"
+  },
+  {
+    "revision": "3b37c7f0a1d10edee5de",
+    "url": "/tip_out_calc/static/js/main.009bd291.chunk.js"
+  },
+  {
+    "revision": "7c1cd2d8e7e253a6dceb",
+    "url": "/tip_out_calc/static/js/runtime-main.10d947ab.js"
+  }
+]);
